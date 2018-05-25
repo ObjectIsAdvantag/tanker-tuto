bbaf0cdc78cf5e5539a4c6c5ffc909a829e141fb
ObjectIsAdvantag <steve.sfartz@gmail.com>
2018-05-25 11:51:44 +0200

    init

---
+++ server.mjs
@@ -0,0 +1,53 @@
+
+import { generateUserToken } from '@tanker/user-token';
+
+// Store these configurations in a safe place
+const trustchainId = '<trustchain-id>';
+const trustchainPrivateKey = '<trustchain-private-key>';
+
+// Example server-side function in which you would implement checkAuth(),
+// retrieveUserToken() and storeUserToken() to use your own authentication
+// and data storage mechanisms:
+function getUserToken(userId) {
+    const isAuthenticated = checkAuth(userId);
+
+    // Always ensure userId is authenticated before returning a user token
+    if (!isAuthenticated) {
+        throw new Error('unauthorized');
+    }
+
+    // Retrieve a previously stored user token for this user
+    let token = retrieveUserToken(userId);
+
+    // If not found, create a new user token
+    if (!token) {
+        token = generateUserToken(trustchainId, trustchainPrivateKey, userId);
+
+        // Store the newly generated user token
+        storeUserToken(userId);
+    }
+
+    // From now, the same user token will always be returned to a given user
+    return token;
+}
+
+// Fake auth
+function checkAuth() {
+    console.log("no authentication")
+    return true;
+}
+
+// No persistent here
+function retrieveUserToken() {
+    console.log("no persistance")
+    return null;
+}
+
+function storeUserToken() {
+    console.log("no persistance")
+}
+
+// Retreive a token for the specified user
+const userId = "12345"
+const token = getUserToken(userId)
+console.log(`NEW token: ${token} for user: ${userId}`)
